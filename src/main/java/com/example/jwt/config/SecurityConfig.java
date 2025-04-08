5db8a0ff03ecd9fec65b8f0b69e54d2f6c2d7d58
choiseongho95 <choish@medicnc.co.kr>
2025-04-08 09:23:00 +0900

    구글 OAUTH2 추가

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.jwt.config;
 import com.example.jwt.filter.JwtAuthenticationFilter;
 import com.example.jwt.handler.CustomAccessDeniedHandler;
 import com.example.jwt.handler.CustomAuthenticationEntryPoint;
+import com.example.jwt.handler.OAuth2SuccessHandler;
 import com.example.jwt.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,21 +21,29 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
     private final CustomAccessDeniedHandler accessDeniedHandler;
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     public SecurityConfig(JwtUtil jwtUtil,
-        CustomAccessDeniedHandler accessDeniedHandler,
-        CustomAuthenticationEntryPoint authenticationEntryPoint) {
+                          CustomAccessDeniedHandler accessDeniedHandler,
+                          CustomAuthenticationEntryPoint authenticationEntryPoint,
+                          OAuth2SuccessHandler oAuth2SuccessHandler) {
         this.jwtUtil = jwtUtil;
         this.accessDeniedHandler = accessDeniedHandler;
         this.authenticationEntryPoint = authenticationEntryPoint;
+        this.oAuth2SuccessHandler = oAuth2SuccessHandler;
     }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/login", "/api/signup", "/api/auth/refresh", "/h2-console/**").permitAll()
+                .requestMatchers("/api/login", "/api/signup", "/api/auth/refresh", "/oauth2/**", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .successHandler(oAuth2SuccessHandler)  // 로그인 성공 시 처리 로직
             )
             .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling(eh -> eh
